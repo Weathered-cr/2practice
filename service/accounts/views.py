85f088918a577a0281e7e1f7cd4adc5600c00380
Илья Дорожко <gr424_doise@lab-402-2.tomtit-tomsk.ru>
2024-10-31 09:30:19 +0700

    guests

--- service/accounts/views.py
+++ service/accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+# Главная страница
+def home(request):
+    return render(request, 'accounts/home.html')
+
+# Вход в систему
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') # Перенаправление после успешного входа
+        else:
+            messages.error(request, 'Неверный логин или пароль.')
+    return render(request, 'accounts/login.html')
+
+# Регистрация
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home') # Перенаправление после успешной регистрации
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
